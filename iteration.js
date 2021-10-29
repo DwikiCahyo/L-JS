89c7fb227179039b9a04fc18ef580b025e2cc5ad
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2021-10-29 23:00:47 +0700

    commit

---
+++ iteration.js
@@ -0,0 +1,28 @@
+// Membuat iterasi dengan for of dan for in
+
+const nama = ['darman','sulis','adam']
+
+
+//menggunakna for biasa
+for(let i = 0; i < nama.length;i++){
+    console.log(nama[i])
+}
+
+//menggunakan foreach
+nama.forEach( m => console.log(m));
+
+//menggunakna for of
+for(const m of nama){
+    console.log(m)
+}
+
+//menggunakan for in (no iterable objek)
+const contoh = {
+    nama : 'fuad',
+    umur : 32,
+}
+
+for ( c in contoh){
+    console.log(contoh[c] ,` indeks adalah  ${c}`)// memanggil isi dari indek contoh[c] , memanggil index c
+}
+
---
+++ testClosure.js
@@ -0,0 +1,26 @@
+// function addSquares() {
+//     let a = 10;
+//     let b = 2;
+//     function square(x) {
+//        return x * x;
+//     }
+//     return square(a) + square(b);
+//  };
+
+
+// console.log(addSquares())
+
+function umur(){
+    let a = 30
+    let b = 31
+    function umurSek(x){
+        return x*2
+    }
+  console.log(umurSek(a),umurSek(b))
+//   console.log(umurSek(b))
+};
+
+umur();
+
+
+ 
