5ce59120477d7860549e502560e40d74cf245f30
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2021-10-29 22:14:22 +0700

    commit

---
+++ arrowFun.js
@@ -0,0 +1,14 @@
+//arrow function
+//Contoh penggunaan seder hana arrow function
+
+// traditional function
+function aba(a){
+    return a+ 100;
+}
+console.log(aba(20))
+
+//arrow function
+ let aba = a => a+100
+
+ console.log(aba(10))
+
